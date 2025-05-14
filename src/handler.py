d9b3dc1f4182cd9193f1ba1b0a35a96396599c35
tpdns90321 <tpdns9032100@gmail.com>
2025-05-14 10:17:08 +0900

    최적화 작업
    1. 불필요한 파일 I/O를 최소화한다. audiofile과 uvr 모델에서는 직접
       파일 주소를 요구하지만 torchaudio에서는 객체로도 사용이 가능하기
       때문에 저장 과정에서 파일로 저장하지 않고 객체로 저장 후 변환했다.
    2. ogg 출력으로 변경, 압축을 조금이나마 해서 빨리 다운로드하도록 생각
    3. 필요한 부분만 다운로드 받도록 필요한 파트를 지정하도록 파라미터 추가
    4. runpod에서는 GPU 사용량이 없고 느려서 cuda 확인 부분을 환경변수가
       아니라 파라미터로 확인해서 지정하도록 변경

--- src/handler.py
+++ src/handler.py
@@ -1,4 +1,4 @@
-""" Example handler file. """
+"""Example handler file."""
 
 import os
 import runpod
@@ -7,42 +7,57 @@ import audiofile
 from uvr import models
 import torchaudio
 import base64
+import torch
 
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless.
 
-device = os.getenv("DEVICE", "cpu")
+if torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cpu"
+print(f"Using device: {device}")
+
 demucs = models.Demucs(
-    name="hdemucs_mmi",
-    other_metadata={"segment": 2, "split": True},
-    device=device)
+    name="hdemucs_mmi", other_metadata={"segment": 2, "split": True}, device=device
+)
+
 
 def handler(job):
-    """ Handler function that will be used to process jobs. """
-    job_input = job['input']
+    """Handler function that will be used to process jobs."""
+    job_input = job["input"]
 
-    audio_base64 = job_input.get('audio')
+    audio_base64 = job_input.get("audio")
     if audio_base64 is None:
         raise ValueError("Missing 'audio' key in input.")
 
-    tmp_audio_file_name = '/tmp/' + str(hash(audio_base64)) + '.mp3'
-    with open(tmp_audio_file_name, 'wb') as audio_file:
-        buffer_ = io.BytesIO(bytes(audio_base64, 'utf-8'))
+    split_parts = job_input.get("parts", ["vocals", "bass", "drums", "other"])
+    print(f"Splitting into parts: {split_parts}")
+
+    tmp_audio_file_name = "/tmp/" + str(hash(audio_base64)) + ".mp3"
+    with open(tmp_audio_file_name, "wb") as audio_file:
+        buffer_ = io.BytesIO(bytes(audio_base64, "utf-8"))
         base64.decode(buffer_, audio_file)
 
-    res = demucs(tmp_audio_file_name)
-    _, rate = audiofile.read(tmp_audio_file_name)
+    try:
+        res = demucs(tmp_audio_file_name)
+        _, rate = audiofile.read(tmp_audio_file_name)
+    finally:
+        os.remove(tmp_audio_file_name)
 
     result = {}
-    split_parts = ['vocals', 'bass', 'drums', 'other']
-    tmp_audio_part_name_template = '/tmp/' + str(hash(audio_base64)) + '{}.mp3'
     for part in split_parts:
+        buffer_ = io.BytesIO()
         torchaudio.save(
-            tmp_audio_part_name_template.format(part), res[part],
-            rate
+            buffer_,
+            res[part],
+            rate,
+            format="ogg",
         )
-        with open(tmp_audio_part_name_template.format(part), 'rb') as pfile:
-            result[part] = base64.b64encode(pfile.read()).decode('utf-8')
+        buffer_.seek(0)
+        result[part] = base64.b64encode(buffer_.read()).decode("utf-8")
 
     return result
 
